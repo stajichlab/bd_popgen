b4962a815b592b5f654dcbea106c2e97e8f906f0
Jason Stajich <jason.stajich@ucr.edu>
2012-12-29 22:32:55 -0800

    data scripts, revisited for new dir structure

---
+++ GO_enrich/LOH/compGOStats_LOH.slim.R
@@ -0,0 +1,112 @@
+# read in the table
+LOH <-read.table("loh_10k_strict_filter_rename.lst",header=F,sep="\t",stringsAsFactors=F, quote="")
+genes <- LOH$V1
+#LOH
+allgenes <- read.csv("BD_trans_to_gene.tab",
+	 header=F,stringsAsFactors=F,sep=" ",quote="")
+universe <- unique(allgenes$V2)
+
+#universe
+#genes
+
+# problem matching mode of this before
+mode(universe)
+mode(genes)
+
+library("AnnotationDbi")
+godat <- read.table("JEL423.IPR.GO_slim", header=F);
+goframeData <- data.frame(godat$V1, godat$V2, godat$V3)
+goFrame <- GOFrame(goframeData,organism="Batrachochytrium dendrobatidis")
+goAllFrame=GOAllFrame(goFrame)
+
+library("GSEABase")
+library("GOstats")
+
+gsc <- GeneSetCollection(goAllFrame, setType = GOCollection())
+
+params <- GSEAGOHyperGParams(name="My Custom GSEA based annot Params",
+          geneSetCollection=gsc,
+	  geneIds = genes,
+	  universeGeneIds = universe,
+	  ontology = "MF",
+	  pvalueCutoff = 0.05,
+	  conditional = FALSE,
+	  testDirection = "over")
+
+
+OverMF <- hyperGTest(params)
+summary(OverMF)
+OverMF
+write.csv(summary(OverMF),"LOH.OverMF_enrich.slim.csv");
+paramsCC <- GSEAGOHyperGParams(name="My Custom GSEA based annot Params",
+          geneSetCollection=gsc,
+	  geneIds = genes,
+	  universeGeneIds = universe,
+	  ontology = "CC",
+	  pvalueCutoff = 0.05,
+	  conditional = FALSE,
+	  testDirection = "over")
+
+OverCC <- hyperGTest(paramsCC)
+summary(OverCC)
+OverCC
+write.csv(summary(OverCC),"LOH.OverCC_enrich.slim.csv");
+
+paramsBP <- GSEAGOHyperGParams(name="My Custom GSEA based annot Params",
+          geneSetCollection=gsc,
+	  geneIds = genes,
+	  universeGeneIds = universe,
+	  ontology = "BP",
+	  pvalueCutoff = 0.05,
+	  conditional = FALSE,
+	  testDirection = "over")
+
+OverBP <- hyperGTest(paramsBP)
+summary(OverBP)
+OverBP
+write.csv(summary(OverBP),"LOH.OverBP_enrich.slim.csv");
+
+
+params <- GSEAGOHyperGParams(name="My Custom GSEA based annot Params",
+          geneSetCollection=gsc,
+          geneIds = genes,
+          universeGeneIds = universe,
+          ontology = "MF",
+          pvalueCutoff = 0.05,
+          conditional = FALSE,
+          testDirection = "under")
+
+
+Under <- hyperGTest(params)
+summary(Under)
+Under
+write.csv(summary(Under),"LOH.UnderMF_enrich.slim.csv");
+
+
+paramsCC <- GSEAGOHyperGParams(name="My Custom GSEA based annot Params",
+          geneSetCollection=gsc,
+          geneIds = genes,
+          universeGeneIds = universe,
+          ontology = "CC",
+          pvalueCutoff = 0.05,
+          conditional = FALSE,
+          testDirection = "under")
+
+UnderCC <- hyperGTest(paramsCC)
+summary(UnderCC)
+UnderCC
+write.csv(summary(UnderCC),"LOH.UnderCC_enrich.slim.csv");
+
+paramsBP <- GSEAGOHyperGParams(name="My Custom GSEA based annot Params",
+          geneSetCollection=gsc,
+          geneIds = genes,
+          universeGeneIds = universe,
+          ontology = "BP",
+          pvalueCutoff = 0.05,
+          conditional = FALSE,
+          testDirection = "under")
+
+UnderBP <- hyperGTest(paramsBP)
+summary(UnderBP)
+UnderBP
+write.csv(summary(UnderBP),"LOH.UnderBP_enrich.slim.csv");
