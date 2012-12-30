94edb8b61ba129e698804466d199322e420c5f39
Jason Stajich <jason.stajich@ucr.edu>
2012-12-29 22:19:12 -0800

    GO setup for Copynumber 0 regions

---
+++ GO_enrich/CNV/compGOStats_CN0.R
@@ -0,0 +1,65 @@
+# read in the table
+CN0 <-read.table("CN0_genes.tab",header=T,sep="\t",stringsAsFactors=F, quote="")
+genes <- CN0$GENE
+
+allgenes <- read.csv("BD_trans_to_gene.tab",
+	 header=F,stringsAsFactors=F,sep=" ",quote="")
+universe <- allgenes$V2
+
+#universe
+#genes
+
+# problem matching mode of this before
+mode(universe)
+mode(genes)
+
+library("AnnotationDbi")
+godat <- read.table("JEL423.IPR.GO",header=F);
+goframeData <- data.frame(godat$V1, godat$V2, godat$V3)
+goFrame <- GOFrame(goframeData,organism="Batrachochytrium dendrobatidis")
+goAllFrame=GOAllFrame(goFrame)
+
+library("GSEABase")
+library("GOstats")
+
+gsc <- GeneSetCollection(goAllFrame, setType = GOCollection())
+
+params <- GSEAGOHyperGParams(name="My Custom GSEA based annot Params",
+          geneSetCollection=gsc,
+	  geneIds = genes,
+	  universeGeneIds = universe,
+	  ontology = "MF",
+	  pvalueCutoff = 0.05,
+	  conditional = FALSE,
+	  testDirection = "over")
+
+
+Over <- hyperGTest(params)
+summary(Over)
+Over
+
+paramsCC <- GSEAGOHyperGParams(name="My Custom GSEA based annot Params",
+          geneSetCollection=gsc,
+	  geneIds = genes,
+	  universeGeneIds = universe,
+	  ontology = "CC",
+	  pvalueCutoff = 0.05,
+	  conditional = FALSE,
+	  testDirection = "over")
+
+OverCC <- hyperGTest(paramsCC)
+summary(OverCC)
+OverCC
+
+paramsBP <- GSEAGOHyperGParams(name="My Custom GSEA based annot Params",
+          geneSetCollection=gsc,
+	  geneIds = genes,
+	  universeGeneIds = universe,
+	  ontology = "BP",
+	  pvalueCutoff = 0.05,
+	  conditional = FALSE,
+	  testDirection = "over")
+
+OverBP <- hyperGTest(paramsBP)
+summary(OverBP)
+OverBP
